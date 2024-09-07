16b3dd83d56e7c3672e53cac7419d7615375be83
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-09-07 14:35:49 -0700

    Initial commit

---
+++ src/ssec_project/hello.py
@@ -0,0 +1,17 @@
+"""hello.py
+Simple hello module for example
+"""
+
+from __future__ import annotations
+
+
+def say_hello() -> str:
+    """
+    Function that prints out a short sentence.
+
+    Returns
+    -------
+    str
+        "Hello world"
+    """
+    return "Hello world."
